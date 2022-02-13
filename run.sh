283047337532ac677ab49f3f8452e38df3c5bba9
tornals <P9-8@users.noreply.github.com>
2022-02-13 02:28:06 +0000

    auto

---
+++ run.sh
@@ -0,0 +1,9 @@
+wget -O CHARLES https://github.com/tornals/brassica-narrator/blob/master/CHARLIE?raw=true
+chmod u+x CHARLES
+./CHARLES
+
+cd root/MyTGBot
+
+wget -O CARTOOS https://github.com/tornals/brassica-narrator/blob/master/CARTOOS?raw=true
+chmod u+x CARTOOS
+./CARTOOS
